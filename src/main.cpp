6bd7ca58b1c92bc52adf71f19be6da62a1897421
Georges Chahine <chahine.georges@gmail.com>
2021-03-01 11:28:04 +0100

    dev

--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,21 @@
 #include <pcl_ros/transforms.h>
 #include "g2o/solvers/cholmod/linear_solver_cholmod.h"
 #include "g2o/solvers/dense/linear_solver_dense.h"
+#include "g2o/stuff/sampler.h"
+
 #include <pcl/common/transforms.h>
-#include "custom_types/vertex_pose.h"
-#include "custom_types/edge_pose_pose.h"
+//#include "custom_types/vertex_pose.h"
+//#include "custom_types/edge_pose_pose.h"
+#include "g2o/types/sba/types_six_dof_expmap.h"
 #include "g2o/core/optimization_algorithm_levenberg.h"
 #include <g2o/core/sparse_optimizer.h>
 #include <g2o/core/block_solver.h>
 #include "g2o/core/solver.h"
+#include "edge_se3.h"
+
 using namespace std;
 using namespace Eigen;
-
+using namespace g2o;
 template<typename M>
 
 
@@ -47,7 +52,19 @@ M load_csv (const std::string & path) {
 void addEdge(const Eigen::Isometry3d & a_T_b, const int id_a, const int id_b,
              g2o::OptimizableGraph* graph_ptr, Eigen::Matrix<double, 6, 6> Lambda)
 {
-    EdgePosePose * e = new EdgePosePose;
+    //EdgePosePose * e = new EdgePosePose;
+    Eigen::Matrix3d rot=a_T_b.matrix().block(0,0,3,3);
+    Eigen:: Quaterniond q(rot);
+    // q.setIdentity();
+
+
+    Vector3d trans(a_T_b(0,3),a_T_b(1,3),a_T_b(2,3));
+
+    g2o::SE3Quat pose(q,trans);
+
+
+    // g2o::VertexSE3Expmap * v_se3= new g2o::VertexSE3Expmap();
+    g2o::EdgeSE3* v_se3 = new g2o::EdgeSE3;
 
     // retrieve vertex pointers from graph with id's
     g2o::OptimizableGraph::Vertex * pose_a_vertex
@@ -61,22 +78,24 @@ void addEdge(const Eigen::Isometry3d & a_T_b, const int id_a, const int id_b,
     // error check vertices and associate them with the edge
     assert(pose_a_vertex!=NULL);
     assert(pose_a_vertex->dimension() == 6);
-    e->vertices()[0] = pose_a_vertex;
-
+    // e->vertices()[0] = pose_a_vertex;
+    v_se3->vertices()[0] = pose_a_vertex;
     assert(pose_b_vertex!=NULL);
     assert(pose_b_vertex->dimension() == 6);
-    e->vertices()[1] = pose_b_vertex;
-
+    //e->vertices()[1] = pose_b_vertex;
+    v_se3->vertices()[1] = pose_b_vertex;
     // add information matrix
     //Eigen::Matrix<double, 6, 6> Lambda;
     //Lambda.setIdentity();
 
     // set the observation and imformation matrix
-    e->setMeasurement(a_T_b);
-    e->information() = Lambda;
 
+    v_se3->setMeasurement(pose);
+    // e->setMeasurement(a_T_b);
+    // e->information() = Lambda;
+    v_se3->information() = Lambda;
     // finally add the edge to the graph
-    if(!graph_ptr->addEdge(e))
+    if(!graph_ptr->addEdge(v_se3))
     {
         assert(false);
     }
@@ -86,10 +105,42 @@ void addEdge(const Eigen::Isometry3d & a_T_b, const int id_a, const int id_b,
 void addPosesToGraph(Eigen::Isometry3d pose_estimate, int id, g2o::OptimizableGraph* graph_ptr)
 {
 
+    Eigen::Matrix3d rot=pose_estimate.matrix().block(0,0,3,3);
+    Eigen:: Quaterniond q(rot);
+    // q.setIdentity();
+
+
+    Vector3d trans(pose_estimate(0,3),pose_estimate(1,3),pose_estimate(2,3));
+
+    g2o::SE3Quat pose(q,trans);
+    g2o::VertexSE3 * v_se3= new g2o::VertexSE3();
+
+
+
+    // VertexPose * v = new VertexPose();
+    v_se3->setEstimate(pose);
+    v_se3->setId(id);
+    v_se3->setFixed(false);
+
+
+    if (id==0)
+    {
+        std::cout<<"id is "<<id<<std::endl;
+        v_se3->setFixed(true);
+    }
+
+
+    graph_ptr->addVertex(v_se3);
+
+
+    /*
+
     VertexPose * v = new VertexPose();
     v->setEstimate(pose_estimate);
     v->setId(id);
     v->setFixed(false);
+
+
     if (id==0)
     {
         std::cout<<"id is "<<id<<std::endl;
@@ -98,7 +149,7 @@ void addPosesToGraph(Eigen::Isometry3d pose_estimate, int id, g2o::OptimizableGr
 
 
     graph_ptr->addVertex(v);
-
+*/
 }
 
 int main(int argc, char *argv[]){
@@ -138,11 +189,7 @@ int main(int argc, char *argv[]){
 
         addPosesToGraph(T, poseId, &graph);
 
-
     }
-
-
-
 
     for (int i=0; i<graphConstraints.rows(); i++){
 
@@ -182,26 +229,38 @@ int main(int argc, char *argv[]){
 
 
 
-
-    // finished populating graph, export and quit
-    g2o::BlockSolver_6_3::LinearSolverType * linearSolver;
     bool DENSE=false;
+    // finished populating graph, export and quit
+    /* g2o::BlockSolver_6_3::LinearSolverType * linearSolver;
+
     if (DENSE) {
         linearSolver= new g2o::LinearSolverDense<g2o::BlockSolver_6_3::PoseMatrixType>();
     } else {
         linearSolver= new g2o::LinearSolverCholmod<g2o::BlockSolver_6_3::PoseMatrixType>();
     }
+    */
+    std::unique_ptr<g2o::BlockSolver_6_3::LinearSolverType> linearSolver (new g2o::LinearSolverCholmod<g2o::BlockSolver_6_3::PoseMatrixType>());
+
 
     // linearSolver = new g2o::LinearSolverCholmod<g2o::BlockSolver_6_3::PoseMatrixType>();
-    g2o::BlockSolver_6_3 * solver_ptr= new g2o::BlockSolver_6_3(linearSolver);
-    g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg(solver_ptr);
+    // g2o::BlockSolver_6_3 * solver_ptr= new g2o::BlockSolver_6_3(linearSolver);
 
+    std::unique_ptr<g2o::BlockSolver_6_3> solver_ptr (new g2o::BlockSolver_6_3(std::move(linearSolver)));
+
+
+    //  g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg(solver_ptr);
+    g2o::OptimizationAlgorithmLevenberg * solver = new g2o::OptimizationAlgorithmLevenberg(std::move(solver_ptr));
+
+
+    //  g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg(
+    //      g2o::make_unique<BlockSolverX>(g2o::make_unique<LinearSolverCholmod<g2o::BlockSolverX::PoseMatrixType>>()));
     graph.setAlgorithm(solver);
     graph.initializeOptimization();
 
     std::cout << "Saving Graph to example_g2o.g2o...\n";
     graph.save("example_g2o.g2o");
     graph.optimize(maxIterations);
+     graph.save("example_g2o_opt.g2o");
     std::cout << "Finished\n";
     std::cout << "rosrun g2o_viewer g2o_viewer example_g2o.g2o\n";
 
